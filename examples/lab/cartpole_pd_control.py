65d944c3183b51a6a2996fb3aee3ebb20490e076
HukoOo <nicky707@hanmail.net>
2024-08-27 11:55:34 +0900

    update: cartpole pd control

---
+++ examples/lab/cartpole_pd_control.py
@@ -0,0 +1,149 @@
+import argparse
+from omni.isaac.lab.app import AppLauncher
+
+# add argparse arguments
+parser = argparse.ArgumentParser(description="Tutorial on using the interactive scene interface.")
+parser.add_argument("--num_envs", type=int, default=2, help="Number of environments to spawn.")
+# append AppLauncher cli args
+AppLauncher.add_app_launcher_args(parser)
+# parse the arguments
+args_cli = parser.parse_args()
+
+# launch omniverse app
+app_launcher = AppLauncher(args_cli)
+simulation_app = app_launcher.app
+
+"""Rest everything follows."""
+
+import torch
+import numpy as np
+import omni.isaac.lab.sim as sim_utils
+from omni.isaac.lab.assets import ArticulationCfg, AssetBaseCfg
+from omni.isaac.lab.scene import InteractiveScene, InteractiveSceneCfg
+from omni.isaac.lab.sim import SimulationContext
+from omni.isaac.lab.utils import configclass
+
+##
+# Pre-defined configs
+##
+from omni.isaac.lab_assets import CARTPOLE_CFG  # isort:skip
+
+
+@configclass
+class CartpoleSceneCfg(InteractiveSceneCfg):
+    """Configuration for a cart-pole scene."""
+
+    # ground plane
+    ground = AssetBaseCfg(prim_path="/World/defaultGroundPlane", spawn=sim_utils.GroundPlaneCfg())
+
+    # lights
+    dome_light = AssetBaseCfg(
+        prim_path="/World/Light", spawn=sim_utils.DomeLightCfg(intensity=3000.0, color=(0.75, 0.75, 0.75))
+    )
+
+    # articulation
+    cartpole: ArticulationCfg = CARTPOLE_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
+
+class PD:
+    def __init__(self, kp_t, kd_t, kp_x, kd_x):
+        self.kp_t = kp_t
+        self.kd_t = kd_t
+        self.kp_x = kp_x
+        self.kd_x = kd_x
+
+    def observe(self, cart_pos, cart_vel, pole_pos, pole_vel):
+        fx = self.kp_t * pole_pos + self.kd_t * pole_vel + self.kp_x * cart_pos + self.kd_x * cart_vel
+        return fx
+
+def run_simulator(sim: sim_utils.SimulationContext, scene: InteractiveScene):
+    """Runs the simulation loop."""
+    # Extract scene entities
+    # note: we only do this here for readability.
+    robot = scene["cartpole"]
+    # Define simulation stepping
+    sim_dt = sim.get_physics_dt()
+    count = 0
+
+    # Define PD controller
+    kp_t = -6000.0
+    kd_t = -15
+    kp_x = 0      # Homework: Tune these gains
+    kd_x = 0
+    controller = PD(kp_t, kd_t, kp_x, kd_x)
+
+    # Simulation loop
+    while simulation_app.is_running():
+        # Reset
+        if count % 500 == 0:
+            # reset counter
+            count = 0
+            # reset the scene entities
+            # root state [pos, quat, lin_vel, ang_vel]
+            # we offset the root state by the origin since the states are written in simulation world frame
+            # if this is not done, then the robots will be spawned at the (0, 0, 0) of the simulation world
+            root_state = robot.data.default_root_state.clone()
+            root_state[:, :3] += scene.env_origins
+            robot.write_root_state_to_sim(root_state)
+            # set joint positions with some noise
+            joint_pos, joint_vel = robot.data.default_joint_pos.clone(), robot.data.default_joint_vel.clone()
+            # joint_pos += torch.rand_like(joint_pos) * 0.1
+            joint_pos += torch.empty(args_cli.num_envs, 2).uniform_(-1, 1).type(torch.FloatTensor)
+            robot.write_joint_state_to_sim(joint_pos, joint_vel)
+            # clear internal buffers
+            scene.reset()
+            print("[INFO]: Resetting robot state...")
+            print(joint_pos)
+
+
+        # Get robot observation
+        joint_pos, joint_vel = robot.data.joint_pos, robot.data.joint_vel
+        cart_pos, pole_pos = joint_pos[:, 0], joint_pos[:, 1]
+        cart_vel, pole_vel = joint_vel[:, 0], joint_vel[:, 1]
+        print(f"Pole angle = {pole_pos}, Pole velocity = {pole_vel}")
+        print(f"Cart position = {cart_pos}, Cart velocity = {cart_vel}")
+
+        # calculate control effort
+        action = controller.observe(cart_pos, cart_vel, pole_pos, pole_vel)
+        print(f"PID action = {action}")
+
+        # Apply random action
+        # # -- generate random joint efforts
+        # efforts = torch.randn_like(robot.data.joint_pos) * 5.0
+        efforts = torch.zeros_like(robot.data.joint_pos)
+        efforts[:, 0] = action
+
+        # -- apply action to the robot
+        print(f"Applying joint efforts = {efforts}\n")
+        robot.set_joint_effort_target(efforts)
+        # -- write data to sim
+        scene.write_data_to_sim()
+        # Perform step
+        sim.step()
+        # Increment counter
+        count += 1
+        # Update buffers
+        scene.update(sim_dt)
+
+def main():
+    """Main function."""
+    # Load kit helper
+    sim_cfg = sim_utils.SimulationCfg(device="cpu", use_gpu_pipeline=False)
+    sim = SimulationContext(sim_cfg)
+    # Set main camera
+    sim.set_camera_view([4.5, 0.0, 6.0], [0.0, 0.0, 2.0])
+    # Design scene
+    scene_cfg = CartpoleSceneCfg(num_envs=args_cli.num_envs, env_spacing=2.0)
+    scene = InteractiveScene(scene_cfg)
+    # Play the simulator
+    sim.reset()
+    # Now we are ready!
+    print("[INFO]: Setup complete...")
+    # Run the simulator
+    run_simulator(sim, scene)
+
+
+if __name__ == "__main__":
+    # run the main function
+    main()
+    # close sim app
+    simulation_app.close()
